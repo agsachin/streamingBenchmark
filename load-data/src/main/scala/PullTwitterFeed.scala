8ba580ebd98766fa53794ac837c3f88fe5cf2850
sachin aggarwal <different.sachin@gmail.com>
2016-02-22 11:11:18 +0530

    working package

---
+++ load-data/src/main/scala/PullTwitterFeed.scala
@@ -0,0 +1,126 @@
+package benchmark.common
+
+import java.util.logging.{Level, Logger}
+
+import com.google.gson.{Gson, GsonBuilder}
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import twitter4j.auth.OAuthAuthorization
+import twitter4j.conf.ConfigurationBuilder
+import benchmark.common.Utils
+
+
+/**
+ * Created by sachin on 2/18/16.
+ */
+
+
+
+class PullTwitterFeed {
+  def main(args: Array[String]) {
+    val commonConfig = Utils.findAndReadConfigFile(args(0), true).asInstanceOf[java.util.Map[String, Any]];
+
+    val consumerKey = commonConfig.get("twitter.consumerKey") match {
+      case s: String => s
+      case other => throw new ClassCastException(other + " not a String")
+    }
+    val consumerSecret = commonConfig.get("twitter.consumerSecret") match {
+      case s: String => s
+      case other => throw new ClassCastException(other + " not a String")
+    }
+    val accessToken = commonConfig.get("twitter.accessToken") match {
+      case s: String => s
+      case other => throw new ClassCastException(other + " not a String")
+    }
+    val accessTokenSecret = commonConfig.get("twitter.accessTokenSecret") match {
+      case s: String => s
+      case other => throw new ClassCastException(other + " not a String")
+    }
+
+
+    val outputDirectory = commonConfig.get("data.outputDirectory") match {
+      case s: String => s
+      case other => throw new ClassCastException(other + " not a String")
+    }
+    val numFilesPerPartition = commonConfig.get("data.numFilesPerPartition") match {
+      case n: Number => n.intValue()
+      case other => throw new ClassCastException(other + " not a Number")
+    }
+    var receivedTweetCount = commonConfig.get("data.receivedTweetCount") match {
+      case n: Number => n.longValue()
+      case other => throw new ClassCastException(other + " not a Number")
+    }
+    val tweetThreshold = commonConfig.get("data.tweetThreshold") match {
+      case n: Number => n.longValue()
+      case other => throw new ClassCastException(other + " not a Number")
+    }
+    val receiverParallalism = commonConfig.get("data.receiverParallalism") match {
+      case n: Number => n.intValue()
+      case other => throw new ClassCastException(other + " not a Number")
+    }
+
+
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+//    val numFilesPerPartition = 1;
+//    var receivedTweetCount: Long = 0;
+//    val tweetThreshold: Long = 1000;
+//    val outputDirectory = "/tmp/output";
+//    val receiverParallalism = 1;
+
+//    val consumerKey = "taVpGWVGyRiFnQDsNwPwITRcH";
+//    val consumerSecret = "3UKCluIj3OuftNhfeXkIyoR0hd8fs2lhAV7Ki5OqGKMc84IpkJ";
+//    val accessToken = "145001241-n8zdD6gi71xWBC8v55eW7BIBQ2uizR21iUCzLGyu";
+//    val accessTokenSecret = "kJsg01s3g9aKGNmbbNgLvzSgCkX0QY3QUbV8XqeWqOAxl";
+
+    val cb = new ConfigurationBuilder()
+    cb.setDebugEnabled(true)
+      .setOAuthConsumerKey(consumerKey)
+      .setOAuthConsumerSecret(consumerSecret)
+      .setOAuthAccessToken(accessToken)
+      .setOAuthAccessTokenSecret(accessTokenSecret)
+
+    val sparkConf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("TwitterPopularTags")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+    val gson: Gson = new GsonBuilder().create();
+
+    val twitterAuth = new OAuthAuthorization(cb.build())
+    val tweetStream =
+      (1 to receiverParallalism).map { _ => TwitterUtils.createStream(ssc, Option(twitterAuth)).map(gson.toJson(_)) }
+    //TwitterUtils.createStream(ssc, Option(twitterAuth)).map(status => status)}
+    // TwitterUtils.createStream(ssc, Option(twitterAuth)).map(gson.toJson(_)) }
+    val unionDStream = ssc.union(tweetStream)
+
+    unionDStream.foreachRDD((rdd, time) => {
+      val count = rdd.count()
+      println("count" + count)
+      if (count > 0) {
+        print("rdd partition=" + rdd.partitions.length)
+        val outputRDD = rdd.repartition(numFilesPerPartition)
+        outputRDD.saveAsTextFile(
+          outputDirectory + "/tweets/" + rdd.id)
+        receivedTweetCount += count
+        if (receivedTweetCount > tweetThreshold) {
+          //sparkConf.set("timeToStop","true");
+          System.exit(0)
+        }
+      }
+    })
+
+    sys.ShutdownHookThread {
+      println("Gracefully stopping Spark Streaming Application")
+      ssc.stop(true, true)
+      println("Application stopped")
+    }
+
+    //tweetStream.repartition(3).saveAsTextFiles(outputDirectory + "/stream/tweets")
+
+    ssc.start()
+    //ssc.stop(sparkConf.getBoolean("timeToStop",false))
+    ssc.awaitTermination()
+  }
+}
---
+++ load-data/src/main/scala/PushToKafka.scala
@@ -0,0 +1,91 @@
+package benchmark.common
+
+import java.util.Properties
+
+import com.google.gson.JsonParser
+import scala.collection.JavaConverters._
+import kafka.producer.{KeyedMessage, Producer, ProducerConfig}
+import benchmark.common.Utils
+import scala.io.Source._
+
+/**
+ * Created by sachin on 2/18/16.
+ */
+
+class PushToKafka{
+  def main(args: Array[String]) {
+    val commonConfig = Utils.findAndReadConfigFile(args(0), true).asInstanceOf[java.util.Map[String, Any]];
+
+//    val brokerListString = commonConfig.get("kafka.brokerListString") match {
+//      case s: String => s
+//      case other => throw new ClassCastException(other + " not a String")
+//    }
+    val serializer = commonConfig.get("kafka.serializer") match {
+      case s: String => s
+      case other => throw new ClassCastException(other + " not a String")
+    }
+    val requiredAcks = commonConfig.get("kafka.requiredAcks") match {
+      case s: String => s
+      case other => throw new ClassCastException(other + " not a String")
+    }
+    val topic = commonConfig.get("kafka.topic") match {
+      case s: String => s
+      case other => throw new ClassCastException(other + " not a String")
+    }
+    val inputDirectory = commonConfig.get("data.inputDirectory") match {
+      case s: String => s
+      case other => throw new ClassCastException(other + " not a String")
+    }
+
+    val kafkaHosts = commonConfig.get("kafka.brokers").asInstanceOf[java.util.List[String]] match {
+      case l: java.util.List[String] => l.asScala.toSeq
+      case other => throw new ClassCastException(other + " not a List[String]")
+    }
+    val kafkaPort = commonConfig.get("kafka.port") match {
+      case n: Number => n.toString()
+      case other => throw new ClassCastException(other + " not a Number")
+    }
+
+    // Create direct kafka stream with brokers and topics
+    val topicsSet = Set(topic)
+    val brokerListString = joinHosts(kafkaHosts, kafkaPort)
+    //    val BROKER_LIST: String = "localhost:9092,localhost:9093,localhost:9094";
+    //    val SERIALIZER: String = "kafka.serializer.StringEncoder";
+    //    val REQUIRED_ACKS: String = "1";
+    //    val KAFKA_TOPIC: String = "test1";
+
+
+    /** Producer properties **/
+    var props: Properties = new Properties()
+    props.put("metadata.broker.list", brokerListString)
+    props.put("auto.offset.reset" , "smallest")
+    props.put("serializer.class", serializer)
+    props.put("request.required.acks", requiredAcks)
+
+    val config: ProducerConfig = new ProducerConfig(props)
+    val producer: Producer[String, String] = new Producer[String, String](config)
+
+    def send(text: String) {
+      val data: KeyedMessage[String, String] = new KeyedMessage[String, String](topic, text);
+      println(text)
+      producer.send(data);
+    }
+
+    fromFile(inputDirectory).getLines.foreach(line => {
+      val text = new JsonParser().parse(line).getAsJsonObject().get("text");
+      //println(text.getAsString)
+      send(text.getAsString)
+    })
+  }
+  def joinHosts(hosts: Seq[String], port: String): String = {
+    val joined = new StringBuilder();
+    hosts.foreach({
+      if (!joined.isEmpty) {
+        joined.append(",");
+      }
+
+      joined.append(_).append(":").append(port);
+    })
+    return joined.toString();
+  }
+}
