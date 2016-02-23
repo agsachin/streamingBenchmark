5f68389313170438b1c9587f3db5fd4288e5769d
sachin aggarwal <different.sachin@gmail.com>
2016-02-23 11:47:37 +0530

    test

--- push-to-kafka/src/main/scala/PushToKafka.scala
+++ push-to-kafka/src/main/scala/PushToKafka.scala
@@ -3,9 +3,10 @@ package benchmark.common.kafkaPush
 import java.util.Properties
 
 import com.google.gson.JsonParser
-import kafka.producer.{KeyedMessage, Producer, ProducerConfig}
-import benchmark.common._
+import benchmark.common.Utils
 import scala.collection.JavaConverters._
+import kafka.producer.{KeyedMessage, Producer, ProducerConfig}
+
 import scala.io.Source._
 
 /**
@@ -16,10 +17,6 @@ object PushToKafka{
   def main(args: Array[String]) {
     val commonConfig = Utils.findAndReadConfigFile(args(0), true).asInstanceOf[java.util.Map[String, Any]];
 
-//    val brokerListString = commonConfig.get("kafka.brokerListString") match {
-//      case s: String => s
-//      case other => throw new ClassCastException(other + " not a String")
-//    }
     val serializer = commonConfig.get("kafka.serializer") match {
       case s: String => s
       case other => throw new ClassCastException(other + " not a String")
@@ -47,11 +44,13 @@ object PushToKafka{
     }
 
     // Create direct kafka stream with brokers and topics
-    val topicsSet = Set(topic)
-     //    val BROKER_LIST: String = "localhost:9092,localhost:9093,localhost:9094";
-    //    val SERIALIZER: String = "kafka.serializer.StringEncoder";
-    //    val REQUIRED_ACKS: String = "1";
-    //    val KAFKA_TOPIC: String = "test1";
+
+//        val brokerListString: String = "localhost:9092,localhost:9093,localhost:9094";
+//        val serializer: String = "kafka.serializer.StringEncoder";
+//        val requiredAcks: String = "1";
+//        val topic: String = "test1";
+//        val inputDirectory="/tmp/data/event";
+
     val brokerListString = new StringBuilder();
 
     for (host <- kafkaBrokers) {
@@ -59,30 +58,31 @@ object PushToKafka{
         brokerListString.append(",")
       }
       brokerListString.append(host).append(":").append(kafkaPort)
-
     }
 
+    println("all props="+" brokerListString.toString()"+ brokerListString.toString()+"serializer"+serializer+"requiredAcks"+requiredAcks)
     /** Producer properties **/
     var props: Properties = new Properties()
     props.put("metadata.broker.list", brokerListString.toString())
     props.put("auto.offset.reset" , "smallest")
     props.put("serializer.class", serializer)
-
+    props.put("request.required.acks", requiredAcks)
 
     val config: ProducerConfig = new ProducerConfig(props)
     val producer: Producer[String, String] = new Producer[String, String](config)
 
     def send(text: String) {
-      val data: KeyedMessage[String, String] = new KeyedMessage[String, String](topic, text);
+      val data: KeyedMessage[String, String] = new KeyedMessage[String, String](topic, text)
       println(text)
       producer.send(data);
     }
 
     fromFile(inputDirectory).getLines.foreach(line => {
-      val text = new JsonParser().parse(line).getAsJsonObject().get("text");
+      val text = new JsonParser().parse(line).getAsJsonObject().get("text")
       //println(text.getAsString)
       Thread.sleep(1)
       send(text.getAsString)
     })
+    producer.close()
   }
 }
