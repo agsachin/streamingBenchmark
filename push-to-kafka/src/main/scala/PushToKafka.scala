852f2328a0505e6ad51316ba4468ec4dda1df0a7
sachin aggarwal <different.sachin@gmail.com>
2016-02-25 17:56:29 +0530

    test

--- push-to-kafka/src/main/scala/PushToKafka.scala
+++ push-to-kafka/src/main/scala/PushToKafka.scala
@@ -8,11 +8,7 @@ import com.google.gson.JsonParser
 import kafka.producer.{KeyedMessage, Producer, ProducerConfig}
 
 import scala.collection.JavaConverters._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent._
-import scala.concurrent.duration._
 import scala.io.Source._
-import scala.util.{Success,Failure}
 
 /**
  * Created by sachin on 2/18/16.
@@ -59,14 +55,10 @@ object PushToKafka {
       case n: Number => n.intValue()
       case other => throw new ClassCastException(other + " not a Number")
     }
-
-    // Create direct kafka stream with brokers and topics
-
-    //        val brokerListString: String = "localhost:9092,localhost:9093,localhost:9094";
-    //        val serializer: String = "kafka.serializer.StringEncoder";
-    //        val requiredAcks: String = "1";
-    //        val topic: String = "test1";
-    //        val inputDirectory="/tmp/data/event";
+    val kafkaPartitions = commonConfig.get("kafka.partitions") match {
+      case n: Number => n.intValue()
+      case other => throw new ClassCastException(other + " not a Number")
+    }
 
     val brokerListString = new StringBuilder();
 
@@ -77,55 +69,81 @@ object PushToKafka {
       brokerListString.append(host).append(":").append(kafkaPort)
     }
 
-    // println("all props="+" brokerListString.toString()"+ brokerListString.toString()+"serializer"+serializer+"requiredAcks"+requiredAcks)
-    /** Producer properties **/
     var props: Properties = new Properties()
     props.put("metadata.broker.list", brokerListString.toString())
     props.put("auto.offset.reset", "smallest")
     props.put("serializer.class", serializer)
     props.put("request.required.acks", requiredAcks)
+//    props.put("partitioner.class", "benchmark.common.kafkaPush.RandomPartitioner");
 
     val config: ProducerConfig = new ProducerConfig(props)
     val producer: Producer[String, String] = new Producer[String, String](config)
+    val r = scala.util.Random
 
-    def send(text: String) {
+
+    def sendTokafka(text: String) {
       //println("Executing task " + text)
-      val data: KeyedMessage[String, String] = new KeyedMessage[String, String](topic, text)
+      val id = r.nextInt(kafkaPartitions)
+      val data: KeyedMessage[String, String] = new KeyedMessage[String, String](topic,id.toString, text )
       // println(text)
 
       producer.send(data);
     }
 
-    def read(x:Long): Long = {
-      println("Executing task " + x)
-      var count: Long = 0
-      while (count <= recordLimitPerThread)
-        fromFile(inputDirectory).getLines.foreach(line => {
-         // println("Executing task " + line)
-          val text = new JsonParser().parse(line).getAsJsonObject().get("text")
-          //println(text.getAsString)
-          // Thread.sleep(1)
-          send(text.getAsString)
-          count += 1
-        })
-      count
-    }
 
-    val tasks: Seq[Future[Long]] = for (i <- 1 to loaderThreads) yield Future {
-      read(i)
-    }
+//
+//    def read(x:Long): Long = {
+//      println("Executing task " + x)
+//      var count: Long = 0
+//      while (count <= recordLimitPerThread)
+//        fromFile(inputDirectory).getLines.foreach(line => {
+//         // println("Executing task " + line)
+//          val text = new JsonParser().parse(line).getAsJsonObject().get("text")
+//          //println(text.getAsString)
+//          // Thread.sleep(1)
+//          send(text.getAsString)
+//          count += 1
+//        })
+//      count
+//    }
 
-    for (task <-tasks){
-        task onComplete {
-          case Success(task) => println(task)
-          case Failure(t) => println("An error has occured: " + t.getMessage)
+    var thread:Array[Thread] = new Array[Thread](loaderThreads+1)
+
+    for (i <- 1 to loaderThreads) {
+       thread(i) = new Thread {
+        override def run {
+          var count: Long = 0
+          while (count <= recordLimitPerThread)
+            fromFile(inputDirectory).getLines.foreach(line => {
+              val text = new JsonParser().parse(line).getAsJsonObject().get("text")
+              println(i)
+              sendTokafka(text.getAsString)
+              count += 1
+            })
         }
+      }
+      thread(i).start
+    }
+    for (i <- 1 to loaderThreads){
+      thread(i).join();
     }
 
-    val aggregated: Future[Seq[Long]] = Future.sequence(tasks)
 
-    val result: Seq[Long] = Await.result(aggregated,awaitTimeForThread.seconds)
-    println("DONE:"+result)
+//    val tasks: Seq[Future[Long]] = for (i <- 1 to loaderThreads) yield Future {
+//      read(i)
+//    }
+//
+//    for (task <-tasks){
+//        task onComplete {
+//          case Success(task) => println(task)
+//          case Failure(t) => println("An error has occured: " + t.getMessage)
+//        }
+//    }
+//
+//    val aggregated: Future[Seq[Long]] = Future.sequence(tasks)
+//
+//    val result: Seq[Long] = Await.result(aggregated,awaitTimeForThread.seconds)
+//    println("DONE:"+result)
 
     producer.close()
   }
